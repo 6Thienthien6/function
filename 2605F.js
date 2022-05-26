dd3a1e150b8f03ec9773e01450e94fc279dfee26
6Thienthien6 <thien502760@gmail.com>
2022-05-26 13:34:38 +0700

    260522

---
+++ 2605F.js
@@ -0,0 +1,24 @@
+// //bài tập 1 ****
+// cách 1
+// // function squareIt(number) {
+// //     return Math.pow(number, 2)
+
+// // }
+// // console.log(squareIt(3))
+// cách 2
+// function squareIt(number) {
+//     return number * number
+
+// }
+// console.log(squareIt(4))
+
+//bài tập 2 ****
+// function areaCircle(radius) {
+//     return Math.PI * 2 * radius
+// }
+// console.log(areaCircle(5))
+
+function perimeterCircle(radius) {
+    return Math.PI * radius * radius
+}
+console.log(perimeterCircle(7))
